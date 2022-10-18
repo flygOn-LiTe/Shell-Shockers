ef35bcf11c2a26391ac83bb043dfbec6c3e4e2aa
Andrew Rowberry <rowbs.js@gmail.com>
2022-10-17 21:33:49 -0700

    Update health-bar.js

--- mods/health-bar/health-bar.js
+++ mods/health-bar/health-bar.js
@@ -24,16 +24,3 @@
     "https://berrywidgets.com/shellshockers/mods/health-bar/health-bar.js";
   document.body.appendChild(script);
 })();
-(function () {
-  var style = document.createElement("link");
-  style.rel = "stylesheet";
-  style.href =
-    "https://berrywidgets.com/shellshockers/mods/health-bar/health-bar.css";
-  document.head.appendChild(style);
-
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src =
-    "https://berrywidgets.com/shellshockers/mods/health-bar/health-bar.js";
-  document.body.appendChild(script);
-})();
